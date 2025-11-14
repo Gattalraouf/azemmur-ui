0112106cda42a97b2c23441d2a7060840b262029
Abderraouf <gattal.raouf@gmail.com>
2025-11-14 23:09:28 +0100

    content: update favicon + add logo

--- apps/www/lib/layout.shared.tsx
+++ apps/www/lib/layout.shared.tsx
@@ -1,8 +1,9 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Logo from '@/components/logo';
 
 export const baseOptions: BaseLayoutProps = {
   nav: {
-    title: 'Azemmur',
+    title: <Logo className='h-8'/>,
   },
   links: [],
 };
