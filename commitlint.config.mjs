2816010c176ba6679055c03feca880e8506441a5
Abderraouf <gattal.raouf@gmail.com>
2025-12-27 14:50:14 +0100

    chore/husky: set up commit-msg hook

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,22 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\w+)\/([\w-]+):\s+(.*)$/,
+      headerCorrespondence: ['type', 'scope', 'subject'],
+    },
+  },
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'perf', 'test', 'ci', 'layout'],
+    ],
+    'scope-empty': [2, 'never'],
+    'scope-case': [2, 'always', 'kebab-case'],
+    'subject-empty': [2, 'never'],
+    'subject-case': [0], // Disabled to allow any casing
+    'subject-max-length': [2, 'always', 72],
+    'header-max-length': [2, 'always', 100],
+  },
+};
