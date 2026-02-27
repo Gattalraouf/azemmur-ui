ec3a982e1489a0e1ed150256689f467bcba9cf07
Abderraouf <gattal.raouf@gmail.com>
2026-02-27 16:51:05 +0100

    feat/floating-dock: add floating dock demo

---
+++ apps/www/registry/demo/components/azemmur/floating-dock/index.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconBrandX,
+  IconMail,
+} from '@tabler/icons-react';
+import {
+  FloatingDock,
+  type DockItemData,
+} from '@/registry/components/azemmur/floating-dock';
+import type { DockVariantProps } from '@/registry/components/azemmur/floating-dock/dock-variants';
+
+const items: DockItemData[] = [
+  {
+    title: 'GitHub',
+    icon: <IconBrandGithub className="size-full" />,
+    href: 'https://github.com',
+  },
+  {
+    title: 'LinkedIn',
+    icon: <IconBrandLinkedin className="size-full" />,
+    href: 'https://linkedin.com',
+  },
+  {
+    title: 'X (Twitter)',
+    icon: <IconBrandX className="size-full" />,
+    href: 'https://x.com',
+  },
+  {
+    title: 'Email',
+    icon: <IconMail className="size-full" />,
+    href: 'bWFpbHRvOmhlbGxvQGV4YW1wbGUuY29t',
+    obfuscated: true,
+  },
+];
+
+export default function FloatingDockDemo({
+  orientation,
+  size,
+  intent,
+  styling,
+  shape,
+  elevation,
+}: DockVariantProps) {
+  return (
+    <div className="flex items-center justify-center h-[400px] w-full">
+      <FloatingDock
+        orientation={orientation}
+        size={size}
+        intent={intent}
+        styling={styling}
+        shape={shape}
+        elevation={elevation}
+      >
+        {items.map((item, i) => (
+          <FloatingDock.Item
+            key={item.title}
+            item={item}
+            tabIndex={i === 0 ? 0 : -1}
+          />
+        ))}
+        <FloatingDock.Separator />
+      </FloatingDock>
+    </div>
+  );
+}
