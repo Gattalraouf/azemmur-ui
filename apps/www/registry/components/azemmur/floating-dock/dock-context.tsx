97915ea6c26d7ceef4313407436e2b14c581aa80
Abderraouf <gattal.raouf@gmail.com>
2026-02-27 16:50:14 +0100

    feat/floating-dock: add floating dock component

---
+++ apps/www/registry/components/azemmur/floating-dock/dock-context.tsx
@@ -0,0 +1,24 @@
+// Copyright (c) 2026 raouf.codes - Azemmur
+
+'use client';
+
+import { MotionValue } from 'motion/react';
+import { createContext, useContext } from 'react';
+import { DockVariantProps } from '@/registry/components/azemmur/floating-dock/dock-variants';
+
+interface DockContextValue extends DockVariantProps {
+  mousePosition: MotionValue<number>;
+  isVertical: boolean;
+}
+
+const DockContext = createContext<DockContextValue | null>(null);
+
+function useDock() {
+  const context = useContext(DockContext);
+  if (!context) {
+    throw new Error('Dock components must be used within <FloatingDock />');
+  }
+  return context;
+}
+
+export { DockContext, useDock, type DockContextValue };
